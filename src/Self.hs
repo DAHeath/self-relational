e7a999ff8c9572ebc5ad8a35d4f4fdb23fa4b697
David Heath <heath.davidanthony@gmail.com>
2019-03-25 14:00:55 -0400

    cleaned up implementaiton

--- src/Self.hs
+++ src/Self.hs
@@ -10,9 +10,7 @@ import qualified Data.Set as S
 import           Data.Set (Set)
 import qualified Data.Map as M
 import           Data.Map (Map)
-import           Data.List (intercalate, nub)
-
-import Debug.Trace
+import           Data.List (intercalate, nub, groupBy)
 
 -- | We just use strings as the space of variables.
 type Var = String
@@ -59,21 +57,19 @@ data Prop
 (\/) p F = p
 (\/) p q = p `Or` q
 
-esubst :: Int -> Map Var Expr -> Expr -> Expr
-esubst i m = \case
+esubst :: Map Var Expr -> Expr -> Expr
+esubst m = \case
   ALit n -> ALit n
-  V v ->
-    let v' = v ++ "_" ++ show i
-    in M.findWithDefault (V v') v m
-  Add a0 a1 -> Add (esubst i m a0) (esubst i m a1)
+  V v -> M.findWithDefault (V v) v m
+  Add a0 a1 -> Add (esubst m a0) (esubst m a1)
 
 esimp :: Expr -> Expr
 esimp (Add (ALit i) (ALit j)) = ALit (i + j)
 esimp (Add e0 e1) = Add (esimp e0) (esimp e1)
 esimp e = e
 
-psubst :: Int -> Map Var Expr -> Prop -> Prop
-psubst i m = \case
+psubst :: Map Var Expr -> Prop -> Prop
+psubst m = \case
   T -> T
   F -> F
   Not p -> Not (go p)
@@ -84,8 +80,8 @@ psubst i m = \case
   Lt e0 e1 -> Lt (goe e0) (goe e1)
   Ge e0 e1 -> Ge (goe e0) (goe e1)
   where
-    go = psubst i m
-    goe = esubst i m
+    go = psubst m
+    goe = esubst m
 
 psimp :: Prop -> Prop
 psimp p =
@@ -194,7 +190,7 @@ looplessEnd = \case
 -- | The space of program states. A state is either a map from variables to the
 -- counter used as a temporary over that variable, or it is a product of two
 -- states.
-data St = Singleton Int (Map Var Expr) | Composite St St
+data St = Singleton (Map Var Expr) | Composite St St
   deriving (Show, Read, Eq, Ord)
 
 -- | The context used to construct fresh variables. We include a counter for
@@ -202,8 +198,16 @@ data St = Singleton Int (Map Var Expr) | Composite St St
 data Ctxt = Ctxt
   { relCount :: Int
   , varCount :: Int
-  , iState :: St
+  , vocab :: [Var]
   } deriving (Show, Read, Eq, Ord)
+
+data Shape = Single | Multi Shape Shape
+  deriving (Show, Read, Eq, Ord)
+
+data Env = Env
+  { quantification :: St -> St
+  , shape :: Shape
+  }
 
 -- | A proposition quantified over a vocabulary.
 data QProp = Forall [Var] Prop
@@ -216,112 +220,118 @@ data QProp = Forall [Var] Prop
 -- A context is maintained to track which variable/relation names are fresh.
 -- An environment is maintained to properly instantiate logical relations with
 -- the correct vocabulary.
-type M a = ReaderT (St -> St) (StateT Ctxt (Writer [QProp])) a
+type M a = ReaderT Env (StateT Ctxt (Writer [QProp])) a
 
-fresh :: St -> M St
-fresh (Composite st0 st1) = Composite <$> fresh st0 <*> fresh st1
-fresh (Singleton _ m) = do
-  c <- state (\ctx -> (varCount ctx, ctx { varCount = varCount ctx + 1 }))
-  pure (Singleton c (M.mapWithKey (\k _ -> V (k ++ "_" ++ show c)) m))
+double :: Monad m => ReaderT Env m a -> ReaderT Env m a
+double = local (\e -> e { shape = Multi (shape e) (shape e) })
 
--- | The core operation of this library. Given a command, a precondition, and a
--- program state (that the precondition is over) the operation computes a
--- postcondition and a new program state (that the postcondition is over).
--- It does this by applying the proof rules for self-relational Hoare logic.
-triple :: Com -> Prop -> St -> M (Prop, St)
-triple c p st = case c of
-  -- Skip has no effect.
-  Skip -> pure (p, st)
-  -- Assign generates a fresh variable and sets this fresh variable equal to
-  -- the expression.
-  Assign x a -> do
-    let Singleton i m = st
-    let a' = esubst i m a
-    let m' = M.insert x a' m
-    pure (p, Singleton i m')
-  -- The effect of an assertion is summarized by the asserted proposition
-  -- itself (over the correct vocabulary).
-  Assert e -> do
-    let Singleton i m = st
-    pure (p /\ psubst i m e, st)
-  -- If the command is a sequence, there are still possibilities for
-  -- optimizations despite there being at least one loop.
-  Seq c0 c1 ->
-    if | loopless c0 || loopless c1 -> do
-          (r, st') <- triple c0 p st
-          triple c1 r st'
-       | looplessStart c0 ->
-        -- If there is a loopless start, we can attempt to chop the start off now.
-          case c0 of
-            -- If the first command is a sequence, then reassociate to move
-            -- towards the beginning of the sequence.
-            Seq c0' c0'' -> triple (Seq c0' (Seq c0'' c1)) p st
-            -- Otherwise, dispatch that first command now (since we know it is not a loop).
-            c -> do
-              (q, st') <- triple c0 p st
-              (r, st'') <- triple c1 q st'
-              pure (r, st'')
-       | looplessEnd c1 ->
-        -- If there is a loopless end, we can attempt to chop the end off now.
-          case c1 of
-            -- If the second command is a sequence, then reassociate to move
-            -- towards the end of the sequence.
-            Seq c1' c1'' -> triple (Seq (Seq c0 c1') c1'') p st
-            -- Otherwise, dispatch that second command now (since we know it is not a loop).
-            c -> do
-              (q, st') <- triple c0 p st
-              (r, st'') <- triple c1 q st'
-              pure (r, st'')
-       | otherwise -> do
-         -- If (1) the sequence contains a loop, (2) the beginning of the
-         -- sequence is a loop, and (3) the end of the sequence is a loop
-         -- then we are in the general case: Both commands in the sequence
-         -- must be considered simultaneously. Apply the seq/prod rule to
-         -- reason about both parts of the sequence together.
-         st1 <- fresh st
-         (q, st') <- triple (Prod Skip c0) (p /\ equiv st st1) (Composite st st1)
-         (r, st'') <- triple (Prod c0 c1) q st'
-         (s, Composite st0' st1') <- triple (Prod c1 Skip) r st''
-         pure (s /\ equiv st0' st1', st0')
-  -- If the command is a sum, we must reason over the two summands separately.
-  Sum c0 c1 -> do
-    (q0, st0) <- triple c0 p st
-    (q1, st1) <- triple c1 p st
-    r <- rel
-    q0 ==> r st0
-    q1 ==> r st1
-    st' <- fresh st0
-    pure (r st', st')
-  -- To handle loops, we apply the loop rule (by constructing a fresh
-  -- relational predicate to represent the loop invariant).
-  Loop c -> do
-    r <- rel
-    p ==> r st
-    st0 <- fresh st
-    (q, st') <- triple c (r st0) st0
-    -- Each loop introduces two new constraints (by applying the consequence
-    -- rule). One states that the precondition implies the loop invariant
-    -- (over the pre-state) and the second states that the body maintains the
-    -- loop invariant.
-    q ==> r st'
-    -- The post-condition of the loop is the loop invariant.
-    pure (r st0, st0)
-  -- Several optimizations are possible in the case of products.
-  Prod c0 c1 ->
-    -- If either multiplicand has no loops, it is sufficient to reason about
-    -- them separately.
-    if loopless c0 || loopless c1
-    then do
+quantifyLeft :: Monad m => St -> ReaderT Env m a -> ReaderT Env m a
+quantifyLeft st = local (\case
+  Env q (Multi _ s) -> Env (q . (st `Composite`)) s)
+
+quantifyRight :: Monad m => St -> ReaderT Env m a -> ReaderT Env m a
+quantifyRight st = local (\case
+  Env q (Multi s _) -> Env (q . (`Composite` st)) s)
+
+fresh :: M St
+fresh = do
+  sh <- asks shape
+  go sh
+  where
+    go (Multi s0 s1) = Composite <$> go s0 <*> go s1
+    go Single = do
+      i <- state (\c -> (varCount c, c { varCount = varCount c + 1 }))
+      voc <- lift $ gets vocab
+      let m = M.fromList (zip voc (map (\v -> V (v ++ "_" ++ show i)) voc))
+      pure (Singleton m)
+
+type Assertion = St -> Prop
+
+(***) :: Assertion -> Assertion -> Assertion
+(***) p q = \case
+  Composite st0 st1 -> p st0 /\ q st1
+  s -> error (show s)
+
+triple :: Assertion -> Com -> Assertion -> M ()
+triple p c q
+  | hasSummary c = do
+     st <- fresh
+     quant <- asks quantification
+     let (r, st') = summary c st
+     (p (quant st) /\ r) ==> q (quant st')
+  | otherwise =
+    case reassociate c of
+      Seq c0 c1 ->
+        if loopless c0 || loopless c1
+        then do
+          r <- rel
+          triple p c0 r
+          triple r c1 q
+        else double $ do
+          st <- fresh
+          r <- rel
+          s <- rel
+          triple (p *** p) (Prod Skip c0) r
+          triple r (Prod c0 c1) s
+          triple s (Prod c1 Skip) (q *** q)
+      Sum c0 c1 -> do
+        triple p c0 q
+        triple p c1 q
+      Loop c -> do
+        st <- fresh
+        quant <- asks quantification
+        p (quant st) ==> q (quant st)
+        triple q c q
+      Prod c0 c1 ->
+        if loopless c0 || loopless c1
+        then do
+          Composite st0 st1 <- fresh
+          r <- rel
+          quantifyRight st1 (triple p c0 r)
+          quantifyLeft st0 (triple r c1 q)
+        else triple p (rewrite c0 c1) q
+
+cseq :: Com -> Com -> Com
+cseq Skip c = c
+cseq c Skip = c
+cseq c0 c1 = Seq c0 c1
+
+reassociate :: Com -> Com
+reassociate c = foldr cseq Skip (map (foldr cseq Skip) groups)
+  where
+    groups = groupBy (\c0 c1 -> hasSummary c0 && hasSummary c1) (deconstruct c)
+    deconstruct (Seq c0 c1) = deconstruct c0 ++ deconstruct c1
+    deconstruct c = [c]
+
+summary :: Com -> St -> (Prop, St)
+summary c st =
+  case c of
+    Skip -> (T, st)
+    Assign x a ->
+      let Singleton m = st
+          a' = esubst m a
+          m' = M.insert x a' m
+      in (T, Singleton m')
+    Assert p ->
+      let Singleton m = st
+       in (psubst m p, st)
+    Seq c0 c1 ->
+      let (p, st') = summary c0 st
+          (q, st'') = summary c1 st'
+      in (p /\ q, st'')
+    Prod c0 c1 ->
       let Composite st0 st1 = st
-      -- Note that we must add state to the environment: Propositions for
-      -- one command are allowed to refer to the vocabulary of its peer.
-      (q0, st0') <- local (. (`Composite` st1)) (triple c0 p st0)
-      (q1, st1') <- local (. (st0' `Composite`)) (triple c1 q0 st1)
-      pure (q1, Composite st0' st1')
-    -- If both multiplicands have loops, then we will apply isomorphisms to
-    -- group loops together.  Eventually the loop/product isomorphism will
-    -- merge loops.
-    else triple (rewrite c0 c1) p st
+          (p, st0') = summary c0 st0
+          (q, st1') = summary c1 st1
+       in (p /\ q, Composite st0' st1')
+
+hasSummary :: Com -> Bool
+hasSummary Skip = True
+hasSummary Assign{} = True
+hasSummary Assert{} = True
+hasSummary (Seq c0 c1) = hasSummary c0 && hasSummary c1
+hasSummary (Prod c0 c1) = hasSummary c0 && hasSummary c1
+hasSummary _ = False
 
 rewrite :: Com -> Com -> Com
 rewrite (Loop c0) (Loop c1) = Loop (Prod c0 c1)
@@ -348,26 +358,23 @@ rewrite c0 c1 = Prod c0 c1
 hoare :: Com -> [QProp]
 hoare c =
   let voc = S.toList $ cvocab c
-      initialState = Singleton 0 (M.fromList (zip voc (map (\v -> V (v ++ "_0")) voc)))
       ctx = Ctxt
         { relCount = 0
         , varCount = 1
-        , iState = initialState
+        , vocab = voc
         }
    in execWriter $ evalStateT (runReaderT
-        (do
-          (q, _) <- triple c T initialState
-          q ==> F) id) ctx
+        (triple (const T) c (const F)) (Env id Single)) ctx
 
 rel :: M (St -> Prop)
 rel = do
   c <- state (\ctx -> (relCount ctx, ctx { relCount = relCount ctx + 1 }))
-  fSt <- ask
+  fSt <- asks quantification
   pure (Rel c . collapse . fSt)
   where
     collapse :: St -> [Expr]
     collapse (Composite st0 st1) = collapse st0 ++ collapse st1
-    collapse (Singleton _ m) = M.elems m
+    collapse (Singleton m) = M.elems m
 
 quantify :: Prop -> QProp
 quantify p = Forall (S.toList (pvocab p)) p
@@ -377,7 +384,7 @@ quantify p = Forall (S.toList (pvocab p)) p
 
 equiv :: St -> St -> Prop
 equiv (Composite st0 st1) (Composite st0' st1') = equiv st0 st0' /\ equiv st1 st1'
-equiv (Singleton _ m0) (Singleton _ m1) =
+equiv (Singleton m0) (Singleton m1) =
   let vs = S.fromList (M.keys m0) `S.union` S.fromList (M.keys m1)
       fetch v = M.findWithDefault (V v) v
    in foldr (/\) T (map (\v -> fetch v m0 `Eql` fetch v m1) (S.toList vs))
@@ -447,7 +454,6 @@ example :: Com
 example =
   Assign "x" (ALit 0) `Seq`
   Assign "x" (Add (V "x") (ALit 1)) `Seq`
-  -- Assign "x" (Add (V "x") (ALit 1)) `Seq`
   Assert (Not (Eql (V "x") (ALit 1)))
 
 loopCopy :: Com
@@ -461,7 +467,7 @@ loopCopy =
             ) (Assert (Ge (V "i") (V "n")))
     ) `Seq`
   Assert (Ge (V "i") (V "n")) `Seq`
-  Assign "i" (ALit 0) `Seq`
+  Assign "i" (ALit 1) `Seq`
   Loop (Sum ( Assert (Lt (V "i") (V "n")) `Seq`
               Assign "s1" (Add (V "s1") (V "i")) `Seq`
               Assign "i" (Add (V "i") (ALit 1))
--- t
+++ t
@@ -1,58 +1,70 @@
 (set-logic HORN)
 (set-option :fixedpoint.engine "duality")
 
-(declare-fun R0 (Int Int Int Int Int Int Int Int) Bool)
+(declare-fun R0 (Int Int Int Int) Bool)
 (declare-fun R1 (Int Int Int Int Int Int Int Int) Bool)
 (declare-fun R2 (Int Int Int Int Int Int Int Int) Bool)
 (declare-fun R3 (Int Int Int Int Int Int Int Int) Bool)
 (declare-fun R4 (Int Int Int Int Int Int Int Int) Bool)
 (declare-fun R5 (Int Int Int Int Int Int Int Int) Bool)
 (declare-fun R6 (Int Int Int Int Int Int Int Int) Bool)
+(declare-fun R7 (Int Int Int Int Int Int Int Int) Bool)
+(declare-fun R8 (Int Int Int Int Int Int Int Int Int Int Int Int) Bool)
+(declare-fun R9 (Int Int Int Int Int Int Int Int Int Int Int Int) Bool)
 
-(assert (forall ((i_1 Int) (n_0 Int) (n_1 Int) (s0_1 Int) (s1_1 Int))
-   (=> (and (= 0 i_1) (= n_0 n_1) (= 0 s0_1) (= 0 s1_1)) (R0 0 n_0 0 0 i_1 n_1 s0_1 s1_1))))
-
-(assert (forall ((i_2 Int) (n_0 Int) (n_2 Int) (s0_2 Int) (s1_2 Int))
-   (=> (and (R0 0 n_0 0 0 i_2 n_2 s0_2 s1_2) (< i_2 n_2)) (R1 0 n_0 0 0 (+ i_2 1) n_2 (+ s0_2 i_2) s1_2))))
-
-(assert (forall ((i_2 Int) (n_0 Int) (n_2 Int) (s0_2 Int) (s1_2 Int))
-   (=> (and (R0 0 n_0 0 0 i_2 n_2 s0_2 s1_2) (>= i_2 n_2)) (R1 0 n_0 0 0 i_2 n_2 s0_2 s1_2))))
-
-(assert (forall ((i_3 Int) (n_0 Int) (n_3 Int) (s0_3 Int) (s1_3 Int))
-   (=> (R1 0 n_0 0 0 i_3 n_3 s0_3 s1_3) (R0 0 n_0 0 0 i_3 n_3 s0_3 s1_3))))
-
-(assert (forall ((i_2 Int) (n_0 Int) (n_2 Int) (s0_2 Int) (s1_2 Int))
-   (=> (and (R0 0 n_0 0 0 i_2 n_2 s0_2 s1_2) (>= i_2 n_2)) (R2 0 n_0 0 0 0 n_2 s0_2 s1_2))))
-
-(assert (forall ((i_4 Int) (i_5 Int) (n_4 Int) (n_5 Int) (s0_4 Int) (s0_5 Int) (s1_4 Int) (s1_5 Int))
-   (=> (and (R2 i_4 n_4 s0_4 s1_4 i_5 n_5 s0_5 s1_5) (< i_4 n_4)) (R3 (+ i_4 1) n_4 (+ s0_4 i_4) s1_4 i_5 n_5 s0_5 s1_5))))
-
-(assert (forall ((i_4 Int) (i_5 Int) (n_4 Int) (n_5 Int) (s0_4 Int) (s0_5 Int) (s1_4 Int) (s1_5 Int))
-   (=> (and (R2 i_4 n_4 s0_4 s1_4 i_5 n_5 s0_5 s1_5) (>= i_4 n_4)) (R3 i_4 n_4 s0_4 s1_4 i_5 n_5 s0_5 s1_5))))
+(assert (forall ((n_1 Int))
+   (R0 0 n_1 0 0)))
 
 (assert (forall ((i_5 Int) (i_6 Int) (n_5 Int) (n_6 Int) (s0_5 Int) (s0_6 Int) (s1_5 Int) (s1_6 Int))
-   (=> (and (R3 i_6 n_6 s0_6 s1_6 i_5 n_5 s0_5 s1_5) (< i_5 n_5)) (R4 i_6 n_6 s0_6 s1_6 (+ i_5 1) n_5 s0_5 (+ s1_5 i_5)))))
+   (=> (and (R0 i_6 n_6 s0_6 s1_6) (R0 i_5 n_5 s0_5 s1_5)) (R3 i_6 n_6 s0_6 s1_6 i_5 n_5 s0_5 s1_5))))
 
-(assert (forall ((i_5 Int) (i_6 Int) (n_5 Int) (n_6 Int) (s0_5 Int) (s0_6 Int) (s1_5 Int) (s1_6 Int))
-   (=> (and (R3 i_6 n_6 s0_6 s1_6 i_5 n_5 s0_5 s1_5) (>= i_5 n_5)) (R4 i_6 n_6 s0_6 s1_6 i_5 n_5 s0_5 s1_5))))
+(assert (forall ((i_4 Int) (i_7 Int) (n_4 Int) (n_7 Int) (s0_4 Int) (s0_7 Int) (s1_4 Int) (s1_7 Int))
+   (=> (R3 i_4 n_4 s0_4 s1_4 i_7 n_7 s0_7 s1_7) (R1 i_4 n_4 s0_4 s1_4 i_7 n_7 s0_7 s1_7))))
 
-(assert (forall ((i_6 Int) (i_7 Int) (n_6 Int) (n_7 Int) (s0_6 Int) (s0_7 Int) (s1_6 Int) (s1_7 Int))
-   (=> (R4 i_6 n_6 s0_6 s1_6 i_7 n_7 s0_7 s1_7) (R2 i_6 n_6 s0_6 s1_6 i_7 n_7 s0_7 s1_7))))
+(assert (forall ((i_4 Int) (i_8 Int) (n_4 Int) (n_8 Int) (s0_4 Int) (s0_8 Int) (s1_4 Int) (s1_8 Int))
+   (=> (and (R1 i_4 n_4 s0_4 s1_4 i_8 n_8 s0_8 s1_8) (< i_8 n_8)) (R1 i_4 n_4 s0_4 s1_4 (+ i_8 1) n_8 (+ s0_8 i_8) s1_8))))
 
-(assert (forall ((i_4 Int) (i_5 Int) (n_4 Int) (n_5 Int) (s0_4 Int) (s0_5 Int) (s1_4 Int) (s1_5 Int))
-   (=> (and (R2 i_4 n_4 s0_4 s1_4 i_5 n_5 s0_5 s1_5) (>= i_4 n_4)) (R5 0 n_4 s0_4 s1_4 i_5 n_5 s0_5 s1_5))))
+(assert (forall ((i_4 Int) (i_9 Int) (n_4 Int) (n_9 Int) (s0_4 Int) (s0_9 Int) (s1_4 Int) (s1_9 Int))
+   (=> (and (R1 i_4 n_4 s0_4 s1_4 i_9 n_9 s0_9 s1_9) (>= i_9 n_9)) (R1 i_4 n_4 s0_4 s1_4 i_9 n_9 s0_9 s1_9))))
 
-(assert (forall ((i_5 Int) (i_8 Int) (n_5 Int) (n_8 Int) (s0_5 Int) (s0_8 Int) (s1_5 Int) (s1_8 Int))
-   (=> (and (R5 i_8 n_8 s0_8 s1_8 i_5 n_5 s0_5 s1_5) (< i_8 n_8)) (R6 (+ i_8 1) n_8 s0_8 (+ s1_8 i_8) i_5 n_5 s0_5 s1_5))))
+(assert (forall ((i_10 Int) (i_11 Int) (n_10 Int) (n_11 Int) (s0_10 Int) (s0_11 Int) (s1_10 Int) (s1_11 Int))
+   (=> (and (R1 i_10 n_10 s0_10 s1_10 i_11 n_11 s0_11 s1_11) (>= i_11 n_11)) (R4 i_10 n_10 s0_10 s1_10 1 n_11 s0_11 s1_11))))
 
-(assert (forall ((i_5 Int) (i_8 Int) (n_5 Int) (n_8 Int) (s0_5 Int) (s0_8 Int) (s1_5 Int) (s1_8 Int))
-   (=> (and (R5 i_8 n_8 s0_8 s1_8 i_5 n_5 s0_5 s1_5) (>= i_8 n_8)) (R6 i_8 n_8 s0_8 s1_8 i_5 n_5 s0_5 s1_5))))
+(assert (forall ((i_12 Int) (i_13 Int) (n_12 Int) (n_13 Int) (s0_12 Int) (s0_13 Int) (s1_12 Int) (s1_13 Int))
+   (=> (R4 i_12 n_12 s0_12 s1_12 i_13 n_13 s0_13 s1_13) (R5 i_12 n_12 s0_12 s1_12 i_13 n_13 s0_13 s1_13))))
 
-(assert (forall ((i_5 Int) (i_9 Int) (n_5 Int) (n_9 Int) (s0_5 Int) (s0_9 Int) (s1_5 Int) (s1_9 Int))
-   (=> (R6 i_9 n_9 s0_9 s1_9 i_5 n_5 s0_5 s1_5) (R5 i_9 n_9 s0_9 s1_9 i_5 n_5 s0_5 s1_5))))
+(assert (forall ((i_15 Int) (i_16 Int) (n_15 Int) (n_16 Int) (s0_15 Int) (s0_16 Int) (s1_15 Int) (s1_16 Int))
+   (=> (and (R5 i_16 n_16 s0_16 s1_16 i_15 n_15 s0_15 s1_15) (< i_16 n_16)) (R6 (+ i_16 1) n_16 (+ s0_16 i_16) s1_16 i_15 n_15 s0_15 s1_15))))
 
-(assert (forall ((i_5 Int) (i_8 Int) (n_5 Int) (n_8 Int) (s0_5 Int) (s0_8 Int) (s1_5 Int) (s1_8 Int))
-   (=> (and (R5 i_8 n_8 s0_8 s1_8 i_5 n_5 s0_5 s1_5) (= i_8 i_5) (= n_8 n_5) (= s0_8 s0_5) (= s1_8 s1_5) (>= i_8 n_8) (not (= s0_8 s1_8))) false)))
+(assert (forall ((i_15 Int) (i_17 Int) (n_15 Int) (n_17 Int) (s0_15 Int) (s0_17 Int) (s1_15 Int) (s1_17 Int))
+   (=> (and (R5 i_17 n_17 s0_17 s1_17 i_15 n_15 s0_15 s1_15) (>= i_17 n_17)) (R6 i_17 n_17 s0_17 s1_17 i_15 n_15 s0_15 s1_15))))
+
+(assert (forall ((i_14 Int) (i_18 Int) (n_14 Int) (n_18 Int) (s0_14 Int) (s0_18 Int) (s1_14 Int) (s1_18 Int))
+   (=> (and (R6 i_14 n_14 s0_14 s1_14 i_18 n_18 s0_18 s1_18) (< i_18 n_18)) (R5 i_14 n_14 s0_14 s1_14 (+ i_18 1) n_18 s0_18 (+ s1_18 i_18)))))
+
+(assert (forall ((i_14 Int) (i_19 Int) (n_14 Int) (n_19 Int) (s0_14 Int) (s0_19 Int) (s1_14 Int) (s1_19 Int))
+   (=> (and (R6 i_14 n_14 s0_14 s1_14 i_19 n_19 s0_19 s1_19) (>= i_19 n_19)) (R5 i_14 n_14 s0_14 s1_14 i_19 n_19 s0_19 s1_19))))
+
+(assert (forall ((i_20 Int) (i_21 Int) (n_20 Int) (n_21 Int) (s0_20 Int) (s0_21 Int) (s1_20 Int) (s1_21 Int))
+   (=> (and (R5 i_20 n_20 s0_20 s1_20 i_21 n_21 s0_21 s1_21) (>= i_21 n_21) (not (= s0_21 s1_21))) (R2 i_20 n_20 s0_20 s1_20 i_21 n_21 s0_21 s1_21))))
+
+(assert (forall ((i_23 Int) (i_24 Int) (n_23 Int) (n_24 Int) (s0_23 Int) (s0_24 Int) (s1_23 Int) (s1_24 Int))
+   (=> (and (R2 i_24 n_24 s0_24 s1_24 i_23 n_23 s0_23 s1_23) (>= i_24 n_24)) (R8 1 n_24 s0_24 s1_24 i_23 n_23 s0_23 s1_23 i_23 n_23 s0_23 s1_23))))
+
+(assert (forall ((i_23 Int) (i_25 Int) (n_23 Int) (n_25 Int) (s0_23 Int) (s0_25 Int) (s1_23 Int) (s1_25 Int))
+   (=> (R8 i_25 n_25 s0_25 s1_25 i_23 n_23 s0_23 s1_23 i_23 n_23 s0_23 s1_23) (R9 i_25 n_25 s0_25 s1_25 i_23 n_23 s0_23 s1_23 i_23 n_23 s0_23 s1_23))))
+
+(assert (forall ((i_23 Int) (i_26 Int) (n_23 Int) (n_26 Int) (s0_23 Int) (s0_26 Int) (s1_23 Int) (s1_26 Int))
+   (=> (and (R9 i_26 n_26 s0_26 s1_26 i_23 n_23 s0_23 s1_23 i_23 n_23 s0_23 s1_23) (< i_26 n_26)) (R9 (+ i_26 1) n_26 s0_26 (+ s1_26 i_26) i_23 n_23 s0_23 s1_23 i_23 n_23 s0_23 s1_23))))
+
+(assert (forall ((i_23 Int) (i_27 Int) (n_23 Int) (n_27 Int) (s0_23 Int) (s0_27 Int) (s1_23 Int) (s1_27 Int))
+   (=> (and (R9 i_27 n_27 s0_27 s1_27 i_23 n_23 s0_23 s1_23 i_23 n_23 s0_23 s1_23) (>= i_27 n_27)) (R9 i_27 n_27 s0_27 s1_27 i_23 n_23 s0_23 s1_23 i_23 n_23 s0_23 s1_23))))
+
+(assert (forall ((i_23 Int) (i_28 Int) (n_23 Int) (n_28 Int) (s0_23 Int) (s0_28 Int) (s1_23 Int) (s1_28 Int))
+   (=> (and (R9 i_28 n_28 s0_28 s1_28 i_23 n_23 s0_23 s1_23 i_23 n_23 s0_23 s1_23) (>= i_28 n_28) (not (= s0_28 s1_28))) (R7 i_28 n_28 s0_28 s1_28 i_23 n_23 s0_23 s1_23))))
+
+(assert (forall ((i_22 Int) (i_29 Int) (n_22 Int) (n_29 Int) (s0_22 Int) (s0_29 Int) (s1_22 Int) (s1_29 Int))
+   (=> (R7 i_22 n_22 s0_22 s1_22 i_29 n_29 s0_29 s1_29) false)))
 
 
 (check-sat)
